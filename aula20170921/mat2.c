3acf03f701deae18ca6a558063b9a5666a0a7b7e
mariestevao <31657254+mariestevao@users.noreply.github.com>
2017-12-07 16:59:48 -0200

    fim de aula

--- aula20170921/mat2.c
+++ aula20170921/mat2.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <iostream>
+#include <conio.h>
 #include <math.h>
-using namespace std;
 
-void main ()
+
+int main ()
 {
 	double lado1,lado2,lado3,ang;
 	printf("Digite dois lados do triangulo.\n");
@@ -14,5 +14,6 @@ void main ()
 	lado3 = pow(lado1, 2) + pow(lado2,2) - (2*lado1*lado2*cos(ang));
 	lado3 = sqrt (lado3);
 	printf("O terceiro lado vale %lf.\n", lado3);	
-	system ("pause");
+	getch ();
+	return 0;
 }
